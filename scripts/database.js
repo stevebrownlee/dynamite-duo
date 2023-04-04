1b14cfc7bd5d1498b0967993382d4f01e430f216
Steve <chortlehoort@gmail.com>
2023-04-04 09:38:54 -0500

    Final version

---
+++ scripts/database.js
@@ -0,0 +1,40 @@
+const database = {
+    heroes: [
+        {
+            id: 1,
+            name: "Dynoguy",
+            power: "Fire blasts"
+        },
+        {
+            id: 2,
+            name: "Hyperspeed",
+            power: "Super speed"
+        }
+    ],
+    villains: [
+        {
+            id: 1,
+            name: "Denim Venim",
+            power: "Venomous pants"
+        },
+        {
+            id: 2,
+            name: "Undertaker",
+            power: "Destructive burrowing"
+        },
+        {
+            id: 3,
+            name: "Sally",
+            power: "Sarcasm"
+        }
+    ]
+}
+
+export const getHeroes = () => {
+    return database.heroes.map(hero => ({...hero}))
+}
+
+export const getVillains = () => {
+    return database.villains.map(villain => ({...villain}))
+}
+
